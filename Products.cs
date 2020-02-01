733e85dd0560b53069da248ba7cbbc2a04edf8a8
DhruvTheCoder <52274310+DhruvTheCoder@users.noreply.github.com>
2020-02-02 00:36:36 +0530

    Create Products.cs class file

---
+++ Products.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertDataUsingApi
+{
+   public class Products
+    {
+        public string name { get; set; }
+
+        public string price { get; set; }
+        public string description { get; set; }
+    }
+}
+
